b6bb9a39ac05b710508316d4b915fb9a5498fd89
AndreMarrero <andremjr65@gmail.com>
2023-09-07 22:25:01 -0400

    complete elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+element.remove(main)
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.textContent="Dre is the champion"
+
+
